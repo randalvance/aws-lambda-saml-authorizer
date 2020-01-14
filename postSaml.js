0c10e5de1b43b3208b56bcab377c68dedf5b0c9a
Randal Vance Cunanan <randal@randalvance.net>
2020-01-14 16:05:15 +0800

    Generate JSON token

--- postSaml.js
+++ postSaml.js
@@ -2,33 +2,42 @@
 
 const AWS = require('aws-sdk');
 const xml2js = require('xml2js');
+const jwt = require('jsonwebtoken');
+
 const sts = new AWS.STS();
 
 module.exports.handler = async (event, context) => {
   console.log('Starting SAML parsing...\n');
-  var arr = event.body.split('&RelayState=');
-  var samlResponse = unescape(arr[0].replace('SAMLResponse=', ''));
-  var samlBuffer = Buffer.from(samlResponse, 'base64').toString('ascii');
-  var result = await xml2js.parseStringPromise(samlBuffer);
+  const arr = event.body.split('&RelayState=');
+  const samlResponse = unescape(arr[0].replace('SAMLResponse=', ''));
+  const samlBuffer = Buffer.from(samlResponse, 'base64').toString('ascii');
+  const result = await xml2js.parseStringPromise(samlBuffer);
 
-  console.log('SAML Response: ');
-  console.log(JSON.stringify(result, null, 2));
+  try {
+    await sts.assumeRoleWithSAML({
+      PrincipalArn: process.env.IDENTITY_PROVIDER_ARN,
+      RoleArn: process.env.ROLE_ARN,
+      SAMLAssertion: samlResponse
+    }).promise();
 
-  const data = await sts.assumeRoleWithSAML({
-    PrincipalArn: process.env.IDENTITY_PROVIDER_ARN,
-    RoleArn: process.env.ROLE_ARN,
-    SAMLAssertion: samlResponse
-  }).promise();
+    const token = jwt.sign({ foo: 'bar' }, 'test');
 
-  console.log(data);
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        input: result
-      },
-      null,
-      2
-    ),
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          jwt: token,
+          saml: result,
+        },
+        null,
+        2
+      ),
+    };
+  } catch(error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: 'An error occured!',
+    };
+  }
 };
