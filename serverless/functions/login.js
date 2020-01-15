12d3de7407827fcef2596015b7141c23373386e5
Randal Vance Cunanan <randal@randalvance.net>
2020-01-15 16:37:11 +0800

    Complete Authentication

--- serverless/functions/login.js
+++ serverless/functions/login.js
@@ -1,12 +1,33 @@
 'use strict';
+const { SAML } = require('passport-saml/lib/passport-saml/saml');
+
+const createRequest = event => ({
+    query: event.queryStringParameters,
+});
 
 module.exports.handler = (event, context, callback) => {
-    const response = {
-        statusCode: 301,
-        headers: {
-            Location: process.env.LOGIN_ENDPOINT,
+    console.log('Logging in...');
+
+    const samlParser = new SAML({
+        entryPoint: process.env.SAML_IDP_URL,
+        callbackUrl: process.env.SAML_CALLBACK_URL,
+        cer: process.env.SAML_IDP_CERT,
+        issuer: event.headers.Host + event.requestContext.path,
+        audience: process.env.SAML_CALLBACK_URL,
+        providerName: 'AWS SAML Lambda Example',
+        signatureAlgorithm: 'sha256'
+    });
+    samlParser.getAuthorizeUrl(createRequest(event), {}, (error, url) => {
+        if (error) {
+            throw error;
         }
-    };
-    
-    callback(null, response);
+        const response = {
+            statusCode: 301,
+            headers: {
+                Location: url,
+            }
+        };
+        
+        callback(null, response);
+    });
 };
--- serverless/functions/postSaml.js
+++ serverless/functions/postSaml.js
@@ -38,6 +38,7 @@ const extractUserInfo = samlObject => {
 module.exports.handler = async (event, context) => {
   console.log('Starting SAML parsing...\n');
   const arr = event.body.split('&RelayState=');
+  const relayState = decodeURIComponent(arr[1]);
   const samlResponse = unescape(arr[0].replace('SAMLResponse=', ''));
   const samlBuffer = Buffer.from(samlResponse, 'base64').toString('ascii');
   const samlObject = await xml2js.parseStringPromise(samlBuffer);
@@ -52,22 +53,15 @@ module.exports.handler = async (event, context) => {
 
     const userInfo = extractUserInfo(samlObject);
 
-    console.log('User Info = ' + userInfo);
-
     const token = jwt.sign(userInfo, 'test', {
       expiresIn: 60 // 30 seconds
     });
 
     return {
-      statusCode: 200,
-      body: JSON.stringify(
-        {
-          jwt: token,
-          saml: samlObject,
-        },
-        null,
-        2
-      ),
+      statusCode: 301,
+      headers: {
+          Location: `${relayState}?token=${encodeURIComponent(token)}`,
+      }
     };
   } catch(error) {
     console.error(error);
