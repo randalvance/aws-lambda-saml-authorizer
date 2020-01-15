af4476a842405c25cf379463db73cfe5e9148971
Randal Vance Cunanan <randal@randalvance.net>
2020-01-15 17:47:10 +0800

    Cors

--- serverless/functions/resource.js
+++ serverless/functions/resource.js
@@ -3,6 +3,9 @@
 module.exports.handler = (event, context, callback) => {
     callback(null, {
         statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
         body: JSON.stringify(event.requestContext.authorizer),
     });
 };
